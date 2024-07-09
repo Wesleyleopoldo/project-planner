519d08e23d3f70359674dd8d0429aa08035ea990
Unknow <Unknow>
2024-07-09 01:24:48 -0300

    Primeira aula da NLW: Se Conectando com o banco de dados em memoria, Creiando os repositories, controllers e os  Requests.

---
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -0,0 +1,46 @@
+package com.rocketseat.planner.trip;
+
+
+import com.rocketseat.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository repository;
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload){
+
+        System.out.println("Recebendo uma requisição do tipo POST...");
+        Trip newTrip = new Trip(payload);
+
+        this.repository.save(newTrip);
+
+        this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
+
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    /**
+     * Faz uma busca no banco de dados para saber os detalhes de uma viagem de algum usuário pelo id da Trip(Viagem)
+     * @param id
+     * @return Retorna uma resposta HTTP de informações vindas do banco de dados...
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
