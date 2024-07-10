b38780faa504a7f1a8563c3e2c2b94f36da35105
Unknow <Unknow>
2024-07-10 00:14:50 -0300

    Criando os metodos da classe ParticipantService e adicionando novos metodos como para viagem, recuperar participantes da viagem no controller da classe Trip.

--- src/main/java/com/rocketseat/planner/participant/ParticipantService.java
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -1,7 +1,9 @@
 package com.rocketseat.planner.participant;
 
+import com.rocketseat.planner.trip.Trip;
 import com.rocketseat.planner.trip.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +12,32 @@ import java.util.UUID;
 @Service
 public class ParticipantService {
 
-    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID tripId){
+    @Autowired
+    ParticipantRepository repository;
 
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip){
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+
+        this.repository.saveAll(participants);
+
+        System.out.println(participants.get(0).getId());
+    }
+
+    public ParticipanteCreateResponse registerParticipantToEvent(String email,Trip trip) {
+        Participant newParticipant = new Participant(email, trip);
+
+        this.repository.save(newParticipant);
+
+        return new ParticipanteCreateResponse(newParticipant.getId());
     }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId){}
+
+    public void triggerConfirmationEmailToParticipant(String email){
+
+    }
+
+    public List<ParticipantDataSet> getAllParticipantsFromEvents(UUID tripId){
+        return this.repository.findByTripId(tripId).stream().map(participant -> new ParticipantDataSet(participant.getId(), participant.getName(), participant.getEmail(), participant.getIsConfirmed())).toList();
+    }
 }
